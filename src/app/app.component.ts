9848ef17c305cd475bda93e34dfb9348034cd2c4
denis.koliban <deniskoliban@gmail.com>
2019-06-24 19:59:06 +0300

    This is the end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,23 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent  implements OnInit{
+export class AppComponent implements OnInit {
   title = 'calendar-app';
-  days:Days[][] = [];
-  startDay:number;
-  month:number = new Date().getMonth();
-  year:number = new Date().getFullYear();
-  calendarMap:Map<string,Days[][]> = new Map<string,Days[][]>();
-  
-  
+  days: Days[][] = [];
+  startDay: number;
+  now = new Date();
+  month: number = this.now.getMonth();
+  year: number = this.now.getFullYear();
+  calendarMap: Map<string, Days[][]> = new Map<string, Days[][]>();
+ 
+
   openPrompt(dayNumber) {
     if (dayNumber === 0) {
       return;
     }
-    const res = prompt('Enter event');
+    let zeroForDay = dayNumber < 10 ? '0' : '';
+    let zeroForMonth = this.month + 1 < 10 ? '0' : '';
+    const res = prompt('Enter event for: ' + zeroForDay + dayNumber + ', ' + zeroForMonth + (this.month + 1) + ', ' + this.year);
     if (res === null) {
       return;
     }
@@ -28,31 +31,35 @@ export class AppComponent  implements OnInit{
     this.days[weekIndex][dayIndex].event = res;
   }
 
-  selectCurrentMonth(){
-    this.month = new Date().getMonth();
+  selectCurrentMonth() {
+    this.year = this.now.getFullYear();
+    this.month = this.now.getMonth();
     this.calendarRoll()
   }
 
-  monthNumber(monthDirection) {
-    if (this.month < 11 && this.month > 0) {
-      this.month = this.month + monthDirection ;
-   }
-   
+  changeMonthNumber(monthDirection) {
+    this.month += monthDirection;
+    if (this.month > 11 ) {
+      this.month = 0;
+      this.year++;
+    } else if (this.month < 0) {
+      this.month = 11;
+      this.year--;
+    }
     this.calendarRoll();
-
   }
 
   calendarRoll() {
-    
+
     const key = this.year.toString() + ' ' + this.month.toString();
     if (!this.calendarMap.has(key)) {
       this.calendarMap.set(key, this.initCalendar(this.year, this.month));
     }
-    
+
     this.days = this.calendarMap.get(key);
   }
 
-  getStartDay(date) { 
+  getStartDay(date) {
     let day = date.getDay();
     if (day == 0) {
       day = 7;
@@ -71,30 +78,28 @@ export class AppComponent  implements OnInit{
     this.startDay = this.getStartDay(new Date(x));
     this.days = [];
     this.days[0] = [];
-    
+
     for (let i = 0; i < 7; i++) {
       if (i < this.startDay) {
-        this.days[0][i] = {day:0, event:""};
+        this.days[0][i] = { day: 0, event: "" };
       }
       else {
-        this.days[0][i] = {day:monthDay++, event:""};
+        this.days[0][i] = { day: monthDay++, event: "" };
       }
-    } 
-    for (; monthDay < dayss+1; monthDay++) {
+    }
+    for (; monthDay < dayss + 1; monthDay++) {
       let weekIndex = Math.floor((monthDay + this.startDay - 1) / 7);
       let dayIndex = (monthDay + this.startDay - 1) % 7;
       if (!this.days[weekIndex]) {
         this.days[weekIndex] = [];
       }
-      this.days[weekIndex][dayIndex] = {day:monthDay, event:""};
+      this.days[weekIndex][dayIndex] = { day: monthDay, event: "" };
     }
-   return this.days;
-   
+    return this.days;
+
   }
 
   ngOnInit() {
     this.initCalendar(this.year, this.month);
-    let str = 'fgfgfg';
-    console.log(15%7)
   }
 }
